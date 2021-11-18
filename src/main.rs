e8ae8908aba0374beb4e2faea25c46ec0df6d2d9
mohammadjavad <mjkmo123@gmail.com>
2021-11-18 20:09:35 +0330

    hmm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::io;
+
 mod util;
 
 fn main() {
 
     loop {
         let x: u8 = util::gen_random();
-        println!("{}", x);
+        let mut input = String::new();
+
+        println!("random number generated !");
+
+        println!("gimme Input :");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("hmm i cant get input");
+
+        let input = input.trim();
+
+        println!("you guess {} i guessed {}", input, x)
     }
 }
 
