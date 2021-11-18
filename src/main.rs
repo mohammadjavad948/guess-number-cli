81922669c95af93481a26b9c94891a3a8840a72f
mohammadjavad <mjkmo123@gmail.com>
2021-11-18 21:48:52 +0330

    push

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,35 @@ fn main() {
 
     println!("{}", Colour::White.paint("-------------------------------"));
 
-    'game_loop: loop {
-        let random_number: u8 = util::gen_random();
-        let mut level: u32 = 1;
-        let mut try_tracker: Vec<bool> = vec![];
+    let mut random_number: u8 = util::gen_random();
+    let mut level: u32 = 1;
+    let mut try_tracker: Vec<bool> = vec![];
 
-        'try_loop: loop {
-            let mut input = String::new();
+    loop {
+        let mut input = String::new();
 
-            util::print_level(&level);
-            util::print_try_count(&try_tracker);
-            util::generate_try_tracker(&level, &try_tracker);
+        util::print_level(&level);
+        util::print_try_count(&try_tracker);
+        util::generate_try_tracker(&level, &try_tracker);
 
-            util::print_guess_and_type();
+        util::print_guess_and_type();
 
-            io::stdin()
-                .read_line(&mut input)
-                .expect("hmm i cant get input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("hmm i cant get input");
 
-            let input: u8 = input.trim().parse().expect("not a string or number");
+        let input: u8 = input.trim().parse().expect("not a string or number");
+
+        if input == random_number {
+            level += 1;
+            try_tracker = vec![];
+            random_number = util::gen_random();
+        } else {
+            try_tracker.push(false);
         }
-
-
-        util::clear_terminal();
     }
+
+
+    util::clear_terminal();
 }
 
