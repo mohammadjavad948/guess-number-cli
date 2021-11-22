33821eabfa16d76a650083d87a31a7aa5020fc05
mohammadjavad <mjkmo123@gmail.com>
2021-11-22 13:14:56 +0330

    hmm

--- src/util.rs
+++ src/util.rs
@@ -120,6 +120,57 @@ impl Game {
         }
     }
 
+
+    fn print_guess_and_type(){
+        println!("{} {}",
+                 Colour::Yellow.paint("Guess And Type Your Number"),
+                 Colour::White.paint("Range 0 <--> 10")
+        );
+    }
+
+    fn print_level(&self){
+        print!("{} {} | ",
+               Colour::Yellow.paint("Level >> "),
+               Colour::Yellow.paint(self.level.to_string())
+        );
+    }
+
+    pub fn print_try_count(&self){
+        print!("{} {} | ",
+               Colour::Purple.paint("Try >> "),
+               Colour::Purple.paint(self.try_tracker.len().to_string())
+        );
+    }
+
+    fn generate_try_base_on_level(&self) -> usize {
+        let tries: isize = (11 - self.level) as isize;
+
+        if tries < 1 {
+            return 3;
+        }
+
+        return tries as usize;
+    }
+
+    fn generate_try_tracker(&self){
+
+        print!("{}", Colour::Blue.paint("Your Tries >> "));
+
+        let tries: Vec<usize> = vec![0; Game::generate_try_base_on_level(&self)];
+        let fails = self.try_tracker.len();
+
+        tries
+            .iter()
+            .map(|x| {
+                if x <= &fails {
+                    print!("{}", Colour::Red.paint("-"))
+                } else {
+                    print!("{}", Colour::White.paint("-"))
+                }
+            });
+    }
+
+
     fn generate_random_number() -> u8 {
         let mut rng = thread_rng();
 
@@ -128,5 +179,14 @@ impl Game {
 
     fn clear_terminal() {
         print!("\x1B[2J\x1B[1;1H");
+    }
+
+    fn end_game(&self){
+        println!("{} {} {} {}",
+            Colour::Red.paint("You Lost At Level :"),
+            Colour::Red.paint(self.level.to_string()),
+            Colour::Red.paint("And Try :"),
+            Colour::Red.paint(self.try_tracker.len().to_string())
+        )
     }
 }
